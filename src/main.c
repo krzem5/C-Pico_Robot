781d9ac0911dff6736a5045ca765d56927de66f2
Krzem <krzem5.dev@gmail.com>
2022-05-25 17:17:18 +0200

    Push Update 25/05/2022, 17:17:18

--- src/main.c
+++ src/main.c
@@ -10,13 +10,10 @@
 #define ULTRASONIC_MAX_DISTANCE_TIME ((uint32_t)(ULTRASONIC_MAX_DISTANCE/ULTRASONIC_SOUND_SPEED_FACTOR*2))
 #define ULTRASONIC_SOUND_SPEED_FACTOR 0.0343f
 #define ULTRASONIC_MAX_WAIT_TICKS 80000
-#define ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT 5
 
 
 
-static uint32_t _ultrasonic_smooth[1<<(ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT+ULTRASONIC_PIN_COUNT_SHIFT)];
-static uint32_t _ultrasonic_smooth_sum[1<<ULTRASONIC_PIN_COUNT_SHIFT];
-static uint32_t _ultrasonic_smooth_index[1<<ULTRASONIC_PIN_COUNT_SHIFT];
+static uint32_t _ultrasonic_values[1<<ULTRASONIC_PIN_COUNT_SHIFT];
 
 
 
@@ -25,14 +22,14 @@ static void _get_distance(void){
 	uint32_t ticks=ULTRASONIC_MAX_WAIT_TICKS;
 	uint32_t start_time[1<<ULTRASONIC_PIN_COUNT_SHIFT];
 	for (unsigned int i=0;i<(1<<ULTRASONIC_PIN_COUNT_SHIFT);i++){
-		start_time[i]=0;
+		_ultrasonic_values[i]=ULTRASONIC_MAX_DISTANCE_TIME;
 	}
 	sleep_us(2);
 	gpio_put(ULTRASONIC_TRIGGER_PIN,1);
 	sleep_us(5);
 	gpio_put(ULTRASONIC_TRIGGER_PIN,0);
 	uint32_t last=0;
-	while (mask){
+	while (mask&&ticks){
 		uint32_t state=gpio_get_all()&mask;
 		uint32_t change=state^last;
 		if (change){
@@ -46,40 +43,19 @@ static void _get_distance(void){
 					start_time[i]=time;
 				}
 				else{
-					start_time[i]=(time-start_time[i])|0x80000000;
+					time-=start_time[i];
+					_ultrasonic_values[i]=(time>ULTRASONIC_MAX_DISTANCE_TIME?ULTRASONIC_MAX_DISTANCE_TIME:time);
 					mask&=~bit;
 				}
 			}
 		}
 		ticks--;
-		if (!ticks){
-			break;
-		}
-	}
-	for (unsigned int i=0;i<(1<<ULTRASONIC_PIN_COUNT_SHIFT);i++){
-		uint32_t time;
-		if (start_time[i]>>31){
-			time=start_time[i]&0x7fffffff;
-			if (time>ULTRASONIC_MAX_DISTANCE_TIME){
-				time=ULTRASONIC_MAX_DISTANCE_TIME;
-			}
-		}
-		else{
-			time=ULTRASONIC_MAX_DISTANCE_TIME;
-		}
-		uint32_t* ptr=_ultrasonic_smooth+(i<<ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT)+_ultrasonic_smooth_index[i];
-		_ultrasonic_smooth_sum[i]+=time-(*ptr);
-		*ptr=time;
-		_ultrasonic_smooth_index[i]=(_ultrasonic_smooth_index[i]+1)&((1<<ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT)-1);
 	}
 }
 
 
 
 int main(){
-	for (uint32_t i=0;i<(1<<(ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT+ULTRASONIC_PIN_COUNT_SHIFT));i++){
-		_ultrasonic_smooth[i]=ULTRASONIC_MAX_DISTANCE_TIME;
-	}
 	stdio_init_all();
 	stdio_usb_init();
 	gpio_init(ULTRASONIC_TRIGGER_PIN);
@@ -87,8 +63,6 @@ int main(){
 	gpio_set_dir(ULTRASONIC_TRIGGER_PIN,GPIO_OUT);
 	gpio_set_dir(PICO_DEFAULT_LED_PIN,GPIO_OUT);
 	for (uint32_t i=0;i<(1<<ULTRASONIC_PIN_COUNT_SHIFT);i++){
-		_ultrasonic_smooth_sum[i]=ULTRASONIC_MAX_DISTANCE_TIME<<ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT;
-		_ultrasonic_smooth_index[i]=0;
 		gpio_init(i);
 		gpio_set_dir(i,GPIO_IN);
 	}
@@ -103,7 +77,7 @@ int main(){
 		uint32_t start=time_us_32();
 		_get_distance();
 		uint32_t end=time_us_32();
-		printf("Distance: %.2f, %.2f -> %u\n",(_ultrasonic_smooth_sum[0]>>ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT)*ULTRASONIC_SOUND_SPEED_FACTOR/2,(_ultrasonic_smooth_sum[1]>>ULTRASONIC_SMOOTH_ARRAY_LENGTH_SHIFT)*ULTRASONIC_SOUND_SPEED_FACTOR/2,(end-start)/1000);
+		printf("Distance: %.2f, %.2f -> %u\n",(_ultrasonic_values[0])*ULTRASONIC_SOUND_SPEED_FACTOR/2,(_ultrasonic_values[1])*ULTRASONIC_SOUND_SPEED_FACTOR/2,(end-start)/1000);
 		sleep_ms(1);
 	}
 	reset_usb_boot(0,0);
